62b2347f25cfc616a4de262f07a18d0fdfed8375
Kaushal Joshi <kaushal_1995@yahoo.ca>
2022-06-30 12:29:49 -0400

    change name space to arbelos, added interface keyword, added various interact methods

--- Runtime/IInteractable.cs
+++ Runtime/IInteractable.cs
@@ -1,15 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-namespace Stemuli
+namespace Arbelos
 {
-    public class IInteractable : MonoBehaviour
+    public interface IInteractable 
     {
-        private AnimationOverrideController animationOverrideController;
-        public virtual IInteractable()
+        protected AnimationOverrideController animationOverrideController;
+        protected UnityEvent OnInteractEnterEvent;
+        protected UnityEvent OnInteractExitEvent;
+        protected UnityEvent OnInteractUpdateEvent;
+
+
+        public virtual Interact() { }
+
+        protected void OnInteractEnter() 
+        { 
+            if(OnInteractEnterEvent != null)
+            {
+                OnInteractEnterEvent.Invoke();
+            }
+        }
+        protected void OnInteractExit() 
         {
-            Debug.Log("Interacting....");
+            if (OnInteractExitEvent != null)
+            {
+                OnInteractExitEvent.Invoke();
+            }
+        }
+        protected void OnInteractUpdate() 
+        {
+            if (OnInteracterUpdateEvent != null)
+            {
+                OnInteracterUpdateEvent.Invoke();
+            }
         }
     }
 }
--- Runtime/IInteractableNetworked.cs
+++ Runtime/IInteractableNetworked.cs
@@ -2,12 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Stemuli
+namespace Arbelos
 {
-    public class IInteractableNetworked : MonoBehaviour
+    public interface IInteractableNetworked : IInteractable
     {
-        private AnimationOverrideController animationOverrideController;
-        public virtual AnimationOverrideController rpc_overrideController;
         public virtual rpc_interact()
         {
             Debug.Log("Interacting over the network....");

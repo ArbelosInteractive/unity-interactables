2cf3c656503c3da6b14a95ea06962cf8bd016a30
Kaushal Joshi <kaushal_1995@yahoo.ca>
2022-07-05 16:55:59 -0400

    Update IInteractableNetworked.cs

--- Runtime/IInteractableNetworked.cs
+++ Runtime/IInteractableNetworked.cs
@@ -2,14 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 namespace Arbelos
 {
     public interface IInteractableNetworked : IInteractable
     {
-        public virtual void RPC_interact()
-        {
-            Debug.Log("Interacting over the network....");
-        }
-
+        public virtual void RPC_Interact();
     }
 }
